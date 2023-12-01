4ba597b4ae7cb3296dfd781135dfc44551b55f5a
Gary Robson <gary@curious12.com>
2023-12-01 08:34:58 +0000

    Add Global Facts Swiper
    Needs ACF post type creating

--- elementor/includes/widgets/c12-stats.php
+++ elementor/includes/widgets/c12-stats.php
@@ -31,32 +31,70 @@ class C12_Stats extends \Elementor\Widget_Base {
     }
 
     protected function register_controls() {
-        
+
     }
 
     protected function render() {
+
+        $facts = get_posts([
+            'post_type' => 'global-fact',
+            'posts_per_page' => -1,
+            'orderby' => 'ID',
+            'order' => 'ASC'
+        ]);
+
         $settings = $this->get_settings_for_display();
         ?>
             <div class="c12-widget c12-stats">
-                
-                <div class="stat">
-                    <div class="number" data-color="blue">
-                        <!-- Javascript span-ify and replace with images - unsure weather to store these as plugin assets... might be for the best rather than clogging media library... -->
-                        <img src="<?= plugins_url('../../assets/img/arr_1_blue.png', __FILE__); ?>">
-                        <img src="<?= plugins_url('../../assets/img/arr_4_blue.png', __FILE__); ?>">
-                        <img src="<?= plugins_url('../../assets/img/arr_3_blue.png', __FILE__); ?>">
+            <div class="slides swiper facts-swiper">
+                <div class="slides-container swiper-wrapper">
+
+                <?php
+                foreach ($facts as $fact) {
+                    $theme = get_field('theme', $fact->ID);
+                    $text_prefix = get_field('text_prefix', $fact->ID);
+                    $value = get_field('value', $fact->ID);
+                    $text_postfix = get_field('text_postfix', $fact->ID);
+
+                    $theme_path = sprintf("../../assets/img/numbers/%s/%s_", $theme, $theme);
+                    ?>
+                    <div class="slide swiper-slide stat">
+                        <?php
+                        if ($text_prefix) {
+                        ?>
+                        <div class="text">
+                            <span><?=$text_prefix?></span>
+                        </div>
+                        <?php } ?>
+                        <div class="number" data-color="<?=$theme?>">
+                            <!-- Javascript span-ify and replace with images - unsure weather to store these as plugin assets... might be for the best rather than clogging media library... -->
+                            <?php
+                            foreach (str_split($value) as $char) {
+                                // check if $char is numeric character
+                                if (is_numeric($char)) {
+                                ?>
+                                    <img src="<?= plugins_url($theme_path.$char.".png", __FILE__); ?>">
+                                <?php } ?>
+                            <?php } ?>
+                        </div>
+                        <?php
+                        if ($text_postfix) {
+                            ?>
+                            <div class="text">
+                                <span><?=$text_postfix?></span>
+                            </div>
+                        <?php } ?>
                     </div>
-                    <div class="text">
-                        <span>New jobs created</span>
-                    </div>
+                    <?php } ?>
                 </div>
 
-                <div class="navigation">
-                    <span class="dot"></span>
-                    <span class="dot"></span>
-                    <span class="dot"></span>
-                    <span class="dot"></span>
-                </div>
+                <div class="pagination navigation"></div>
+<!--                <div class="navigation">-->
+<!--                    --><?php
+//                    foreach ($facts as $fact) { ?>
+<!--                    <span class="dot dot_--><?php //=get_field('theme', $fact->ID)?><!--"></span>-->
+<!--                    --><?php //} ?>
+<!--                </div>-->
             </div>
         <?php
     }
--- src/js/modules/swipers.js
+++ src/js/modules/swipers.js
@@ -29,5 +29,33 @@ export default () => {
           },
     })
 
+    const factSwiper = new Swiper('.facts-swiper', {
+        modules: [Navigation, Pagination],
+        pagination: {
+            el: ".pagination",
+            type: "custom",
+            renderCustom: function (swiper, current, total) {
+                var paginationHtml = '';
+                for (var i = 0; i < total; i++) {
+                    var className = 'dot swiper-pagination-bullet';
+                    if (i === current - 1) {
+                        className += 'dot swiper-pagination-bullet-active';
+                    }
+                    paginationHtml += '<span class="' + className + '"></span>';
+                }
+
+                // Combine the progress bar and pagination numbers
+                return paginationHtml;
+            }
+
+        },
+        navigation: {
+            nextEl: '.next',
+            prevEl: '.prev',
+        },
+    })
+
+
+
 
 }
